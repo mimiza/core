9aa9d70f0d9cc048e683df1572479ad4f5fab24c
mimiza <dev@mimiza.com>
2021-03-24 18:36:31 +0700

    init

--- framework.js
+++ framework.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', e => {
-    
+
     render()
 })
 
 const render = () => {
+    head()
     const links = document.body.querySelectorAll('[href][data-link]')
     links.map(e => {
         e.addEventListener('click', e => {
@@ -14,9 +15,12 @@ const render = () => {
 }
 
 const head = () => {
-    
+    if (!document.head.querySelector('meta[charset]'))
+        document.head.prepend(document.createElement('meta').setAttribute('charset', 'UTF-8'))
+    if (!document.head.querySelector('meta[viewport]'))
+        document.head.prepend(document.createElement('meta').setAttribute('name', 'viewport').setAttribute('content', 'width=device-width, initial-scale=1'))
 }
 
 const redirect = () => {
-    
+
 }
