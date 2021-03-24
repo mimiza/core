6d1ae4be4590882647112afa6cb4136e6694b5b6
MIMIZA <dev@mimiza.com>
2021-03-24 18:50:18 +0700

    next

--- framework.js
+++ framework.js
@@ -1,26 +1,33 @@
-document.addEventListener('DOMContentLoaded', e => {
-
+document.addEventListener("DOMContentLoaded", (e) => {
     render()
 })
 
 const render = () => {
     head()
-    const links = document.body.querySelectorAll('[href][data-link]')
-    links.map(e => {
-        e.addEventListener('click', e => {
-            e.preventDefault()
-            console.log('link clicked', e, this)
-        })
-    })
+    const links = document.body.querySelectorAll("[href][data-link]")
+
+    // links.map(e => {
+    //     e.addEventListener('click', e => {
+    //         e.preventDefault()
+    //         console.log('link clicked', e, this)
+    //     })
+    // })
 }
 
 const head = () => {
-    if (!document.head.querySelector('meta[charset]'))
-        document.head.prepend(document.createElement('meta').setAttribute('charset', 'UTF-8'))
-    if (!document.head.querySelector('meta[viewport]'))
-        document.head.prepend(document.createElement('meta').setAttribute('name', 'viewport').setAttribute('content', 'width=device-width, initial-scale=1'))
+    if (!document.head.querySelector("meta[charset]")) {
+        var tmp = document.createElement("meta")
+        tmp.setAttribute("charset", "UTF-8")
+        document.head.append(tmp)
+    }
+    if (!document.head.querySelector("meta[viewport]")) {
+        var tmp = document.createElement("meta")
+        Object.assign(tmp, {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1",
+        })
+        document.head.append(tmp)
+    }
 }
 
-const redirect = () => {
-
-}
+const redirect = () => {}
